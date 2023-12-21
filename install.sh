3621ec26e86c7795724839094a2dd96385c882e8
RED BİTH <136892734+Red-BITH@users.noreply.github.com>
2023-12-21 06:49:53 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@ clear
 echo "******* Torghost installer ********"
 tput setaf 1; echo "Torghost made by Technical Dada"
 tput setaf 2; echo "=====> Installing tor bundle "
-sudo apt-get install tor -y -qq
+apt-get install tor -y -qq
 echo "=====> Installing dependencies "
-sudo pip install stem
+pip install stem
 echo "=====> Installing TorGhost "
-sudo cp torghost /usr/bin/torghost
-sudo chmod +x /usr/bin/torghost
+cp torghost /usr/bin/torghost
+chmod +x /usr/bin/torghost
 echo "=====> Done "
 echo "=====> Open terminal and type 'torghost' for usage "
